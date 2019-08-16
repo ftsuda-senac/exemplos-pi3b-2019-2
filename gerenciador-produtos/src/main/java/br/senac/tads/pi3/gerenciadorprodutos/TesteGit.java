f8751f104778e6b288e38bfc103f1707ddd2f21b
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-08-15 22:08:54 -0300

    Alteração msg

--- gerenciador-produtos/src/main/java/br/senac/tads/pi3/gerenciadorprodutos/TesteGit.java
+++ gerenciador-produtos/src/main/java/br/senac/tads/pi3/gerenciadorprodutos/TesteGit.java
@@ -13,7 +13,7 @@ public class TesteGit {
     
     public static void main(String[] args) {
         
-        System.out.println("Teste de commits");
+        System.out.println("    OLÁ MUNDO");
         
     }
     
